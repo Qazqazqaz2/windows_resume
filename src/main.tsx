7f7bb1749a06e4f42c878cb1daac3badd1e29c00
Qazqazqaz2 <41629780+Qazqazqaz2@users.noreply.github.com>
2026-02-27 20:32:05 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ const basename = import.meta.env.BASE_URL;
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
